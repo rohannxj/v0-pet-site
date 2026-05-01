17bb9642f5b648a51743a1e32c4077dda7c4a76f
Rohan Najran <rohannajran@outlook.com>
2026-05-01 11:45:11 +0100

    efore ui change

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,15 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 }
 
 export default nextConfig
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+//import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
